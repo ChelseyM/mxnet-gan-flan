289ff4d5b36a459883c156c94f7a26b73b8d566a
ChelseyM <m.chelsey@gmail.com>
2019-07-25 08:41:20 +0200

    Updated version

--- project.clj
+++ project.clj
@@ -19,7 +19,7 @@
   :description "GAN Flans with MXNet"
   :plugins [[lein-cljfmt "0.5.7"]]
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.apache.mxnet.contrib.clojure/clojure-mxnet-osx-cpu "1.3.0"]
+                 [org.apache.mxnet.contrib.clojure/clojure-mxnet "1.5.0-SNAPSHOT"]
                  [nu.pattern/opencv "2.4.9-7"]
                  [net.mikera/imagez "0.12.0"]
                  [thinktopic/think.image "0.4.16"]]
